e1f7fdb99e48368acb53d605d60e9c6fcd9f298d
Efrain <carlosefrain17@hotmail.com>
2018-08-25 10:34:43 -0500

    D-1 nuevo client

--- deliktum/client/src/app/app.component.ts
+++ deliktum/client/src/app/app.component.ts
@@ -1,13 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  encapsulation: ViewEncapsulation.None,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app'; 
-  
+  title = 'client';
 }
