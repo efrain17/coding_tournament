87d1136ce8f931c3fd91701450e7b6565eeb0e26
Efrain <carlosefrain17@hotmail.com>
2018-08-25 10:48:27 -0500

    D-2 componentes base

--- deliktum/client/src/app/app.module.ts
+++ deliktum/client/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MapComponent } from './map/map.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MapComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
